24a06494e005dd1106803895f8d68c919e105e74
Bond <lovedammy77@yahoo.com>
2019-02-19 14:43:50 +0100

    Update build config

--- rollup.config.js
+++ rollup.config.js
@@ -1,43 +1,122 @@
 import resolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
 import babel from 'rollup-plugin-babel';
+import replace from 'rollup-plugin-replace';
+import { sizeSnapshot } from 'rollup-plugin-size-snapshot';
+import { uglify } from 'rollup-plugin-uglify';
+
 import pkg from './package.json';
 
-export default [
-  // browser-friendly UMD build
-  {
-    input: 'src/index.js',
-    output: {
-      file: pkg.browser,
-      format: 'umd',
-      name: 'ReactEpicSpinners',
-    },
-    plugins: [
-      resolve(),
-      commonjs(),
-      babel({
-        exclude: ['node_modules/**'],
-      }),
-    ],
-    external: ['react', 'prop-types', 'styled-components'],
-  },
+const input = 'src/index.js';
+const globalName = 'ReactEpicSpinners';
 
-  // CommonJS (for Node) and ES module (for bundlers) build.
-  // (We could have three entries in the configuration array
-  // instead of two, but it's quicker to generate multiple
-  // builds from a single configuration where possible, using
-  // the `targets` option which can specify `dest` and `format`)
+function external(id) {
+  return !id.startsWith('.') && !id.startsWith('/');
+}
+
+const cjs = [
   {
-    input: 'src/index.js',
-    output: [
-      { file: pkg.main, format: 'cjs' },
-      { file: pkg.module, format: 'es' },
-    ],
+    input,
+    output: { file: `dist/${pkg.name}.cjs.js`, format: 'cjs' },
+    external,
+    plugins: [
+      babel({ exclude: /node_modules/ }),
+      replace({ 'process.env.NODE_ENV': JSON.stringify('development') })
+    ]
+  },
+  {
+    input,
+    output: { file: `dist/${pkg.name}.cjs.min.js`, format: 'cjs' },
+    external,
+    plugins: [
+      babel({ exclude: /node_modules/ }),
+      replace({ 'process.env.NODE_ENV': JSON.stringify('production') }),
+      uglify()
+    ]
+  }
+];
+
+const esm = [
+  {
+    input,
+    output: { file: `dist/${pkg.name}.esm.js`, format: 'esm' },
+    external,
     plugins: [
       babel({
-        exclude: ['node_modules/**'],
+        exclude: /node_modules/,
+        runtimeHelpers: true,
+        plugins: [['@babel/transform-runtime', { useESModules: true }]]
       }),
-    ],
-    external: ['react', 'prop-types', 'styled-components'],
-  },
+      sizeSnapshot()
+    ]
+  }
 ];
+
+const globals = {
+  react: 'React',
+  'prop-types': 'PropTypes',
+  'styled-components': 'StyledComponents'
+};
+
+const umd = [
+  {
+    input,
+    output: {
+      file: `dist/${pkg.name}.umd.js`,
+      format: 'umd',
+      name: globalName,
+      globals
+    },
+    external,
+    plugins: [
+      babel({
+        exclude: /node_modules/,
+      }),
+      resolve(),
+      commonjs({
+        include: /node_modules/
+      }),
+      replace({ 'process.env.NODE_ENV': JSON.stringify('development') }),
+      sizeSnapshot()
+    ]
+  },
+  {
+    input,
+    output: {
+      file: `dist/${pkg.name}.umd.min.js`,
+      format: 'umd',
+      name: globalName,
+      globals
+    },
+    external,
+    plugins: [
+      babel({
+        exclude: /node_modules/,
+      }),
+      resolve(),
+      commonjs({
+        include: /node_modules/
+      }),
+      replace({ 'process.env.NODE_ENV': JSON.stringify('production') }),
+      sizeSnapshot(),
+      uglify()
+    ]
+  }
+];
+
+let config;
+switch (process.env.BUILD_ENV) {
+  case 'cjs':
+    config = cjs;
+    break;
+  case 'esm':
+    config = esm;
+    break;
+  case 'umd':
+    config = umd;
+    break;
+  default:
+    config = cjs.concat(esm).concat(umd);
+}
+
+export default config;
