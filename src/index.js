66f8fd25b66e17bf62de3a2106eb74f1b843317d
Bond <lovedammy77@yahoo.com>
2021-02-23 23:20:44 +0100

    Format index file

--- src/index.js
+++ src/index.js
@@ -1,54 +1,34 @@
 export { default as AtomSpinner } from './components/AtomSpinner';
 
-export {
-  default as BreedingRhombusSpinner,
-} from './components/BreedingRhombusSpinner';
+export { default as BreedingRhombusSpinner } from './components/BreedingRhombusSpinner';
 
-export {
-  default as CirclesToRhombusesSpinner,
-} from './components/CirclesToRhombusesSpinner';
+export { default as CirclesToRhombusesSpinner } from './components/CirclesToRhombusesSpinner';
 
 export { default as FingerprintSpinner } from './components/FingerprintSpinner';
 export { default as FlowerSpinner } from './components/FlowerSpinner';
 
-export {
-  default as FulfillingBouncingCircleSpinner,
-} from './components/FulfillingBouncingCircleSpinner';
+export { default as FulfillingBouncingCircleSpinner } from './components/FulfillingBouncingCircleSpinner';
 
-export {
-  default as FulfillingSquareSpinner,
-} from './components/FulfillingSquareSpinner';
+export { default as FulfillingSquareSpinner } from './components/FulfillingSquareSpinner';
 
 export { default as HalfCircleSpinner } from './components/HalfCircleSpinner';
 export { default as HollowDotsSpinner } from './components/HollowDotsSpinner';
 
-export {
-  default as IntersectingCirclesSpinner,
-} from './components/IntersectingCirclesSpinner';
+export { default as IntersectingCirclesSpinner } from './components/IntersectingCirclesSpinner';
 
-export {
-  default as LoopingRhombusesSpinner,
-} from './components/LoopingRhombusesSpinner.js';
+export { default as LoopingRhombusesSpinner } from './components/LoopingRhombusesSpinner';
 
 export { default as OrbitSpinner } from './components/OrbitSpinner';
 export { default as PixelSpinner } from './components/PixelSpinner';
 export { default as RadarSpinner } from './components/RadarSpinner';
 
-export {
-  default as ScalingSquaresSpinner,
-} from './components/ScalingSquaresSpinner';
+export { default as ScalingSquaresSpinner } from './components/ScalingSquaresSpinner';
 
-export {
-  default as SelfBuildingSquareSpinner,
-} from './components/SelfBuildingSquareSpinner';
+export { default as SelfBuildingSquareSpinner } from './components/SelfBuildingSquareSpinner';
 
 export { default as SemipolarSpinner } from './components/SemipolarSpinner';
 export { default as SpringSpinner } from './components/SpringSpinner';
 
-export {
-  default as SwappingSquaresSpinner,
-} from './components/SwappingSquaresSpinner';
+export { default as SwappingSquaresSpinner } from './components/SwappingSquaresSpinner';
 
-export {
-  default as TrinityRingsSpinner,
-} from './components/TrinityRingsSpinner';
+export { default as TrinityRingsSpinner } from './components/TrinityRingsSpinner';
